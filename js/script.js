80dfa9818bbd7dd6508889c07b9469a9a124d9ed
ribo <roibeard.mac.unfraidh@gmail.com>
2021-01-29 12:11:45 +0100

    Add initial project content to work.html.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,13 @@ console.log('in')
 		$('#banba-vid').attr('src','./fís/banba2.mp4')
 	});
 
+	
+
+	$("#banba").mouseleave(function(){
+		console.log('out')
+				$('#banba-vid').attr('src','./fís/banba.mp4')
+			});
+
  });
 
 
