665def5ff90d2bdbc50f177de5ace089673dede6
ribo <roibeard.mac.unfraidh@gmail.com>
2021-01-29 22:58:10 +0100

    Reconnect script.js with work.html.

--- js/script.js
+++ js/script.js
@@ -2,31 +2,32 @@
 $(document).ready(function () {
 
 
-	$("#banba").mouseover(function(){
-console.log('in')
-		$('#banba-vid').attr('src','./fís/banba2.mp4')
+	$("#banba").mouseover(function () {
+		// alert();
+		console.log('in')
+		$('#banba-vid').attr('src', './vids/banba2.mp4')
 	});
 
-	
 
-	$("#banba").mouseleave(function(){
+
+	$("#banba").mouseleave(function () {
 		console.log('out')
-				$('#banba-vid').attr('src','./fís/banba.mp4')
-			});
-			$(".geaga").mouseover(function(){
-				console.log('in')
-						$('#geaga').attr('src','./img/countZero2.png')
-					});
-				
-					
-				
-					$(".geaga").mouseleave(function(){
-						console.log('out')
-								$('#geaga').attr('src','./img/countZero.png')
-							});
+		$('#banba-vid').attr('src', './vids/banba.mp4')
+	});
+	$(".geaga").mouseover(function () {
+		console.log('in')
+		// $('#geaga').attr('src', './img/countZero2.png')
+	});
 
 
- });
+
+	$(".geaga").mouseleave(function () {
+		console.log('out')
+		$('#geaga').attr('src', './img/countZero.png')
+	});
+
+
+});
 
 
 
